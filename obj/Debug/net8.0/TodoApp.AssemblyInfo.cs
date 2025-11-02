51a225a4d199774203c29cde520a4fbb933f786b
imnasim31415 <imnasim3.1415@gmail.com>
2025-11-02 08:12:54 +0600

    layout changed to minimal

--- obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e022e4a256d2c31c721c7ef1514b2056e70fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cbe820f6f6bbd8bccec01dcea37292c2c75e6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
